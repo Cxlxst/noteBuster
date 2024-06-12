00b9317523cc21c736ab5ee020ffd9037a1401ed
Cxlxst <celeste.terpin@outlook.com>
2024-06-12 10:54:13 +0200

    add little css

--- src/personnes/listePersonne.jsx
+++ src/personnes/listePersonne.jsx
@@ -1,10 +1,11 @@
 import { FichePersonne } from "./fichePersonne/FichePersonne"
+import "./listePersonne.css"
 
 export const ListePersonne = ({liste}) => {
 
     return (
         
-        <div>
+        <div class="listePersonne">
             {
                 liste.map((value, index)=>{
                     return <FichePersonne key={index} nom={value.nom} prenom={value.prenom} image={value.image} dateNaissance={value.dateNaissance} genre={value.genre}/>
